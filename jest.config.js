e70678ef86b05394e49633648a188b40cb4aa378
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-06-17 13:23:14 +0100

    configure jsdom URL to enable navigation  🐿 v2.12.3

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	testURL: 'http://localhost/',
 	testEnvironment: 'jest-environment-jsdom-fifteen',
 	testPathIgnorePatterns: ['/dist/', '/node_modules/', '/bower_components/']
 };
