2216bf47d915d4f27296b56f36da729bde4d15d1
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-07-18 18:15:59 +0100

    Delete old error page context info code

--- src/utilities/error.js
+++
@@ -1,9 +0,0 @@
-import { findInQueryString } from './dom';
-
-export function getErrorStatus () {
-	return findInQueryString('nextErrorStatus');
-}
-
-export function getErrorReason () {
-	return findInQueryString('nextErrorReason');
-}
