3b52aa06763774061cd1a6cd16a178ae5e4ffa0c
Matt Hinchliffe <matt@maketea.co.uk>
2019-08-27 09:07:02 +0100

    Port core tracking template over from Page Kit shell component package
     🐿 v2.12.4

---
+++ src/server/components/CoreTracking.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const pixel = 'https://spoor-api.ft.com/px.gif?data=';
+
+const placeholder = '[SOURCE]';
+
+export function CoreTracking({ appContext }) {
+	// const context = filterAppContext(appContext);
+
+	const trackingData = {
+		category: 'page',
+		action: 'view',
+		system: {
+			source: 'non-ctm'
+		},
+		context: {
+			// ...context,
+			product: 'next',
+			data: { source: placeholder }
+		}
+	};
+
+	const encodedTrackingData = encodeURIComponent(JSON.stringify(trackingData));
+
+	// NOTE: This function will be stringified and embedded so use ES5 only!
+	function coreExperience() {
+		if (/\bcore\b/.test(document.documentElement.className)) {
+			var currentScript = document.scripts[document.scripts.length - 1];
+			var img = new Image();
+
+			img.src = currentScript.getAttribute('data-pixel-src');
+		}
+	}
+
+	return (
+		<React.Fragment>
+			<script
+				// To pass this information to the client-side
+				data-pixel-src={
+					pixel + encodedTrackingData.replace(placeholder, 'core-experience')
+				}
+				dangerouslySetInnerHTML={{
+					__html: '(' + coreExperience.toString() + ')();'
+				}}
+			/>
+			<noscript>
+				<img src={pixel + encodedTrackingData.replace(placeholder, 'no-js')} />
+			</noscript>
+		</React.Fragment>
+	);
+}
