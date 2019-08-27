092a685169f5b9d5e5738afa3508367252ce7e34
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-08-27 15:35:26 +0100

    Integrate server-side components with Rollup build
     🐿 v2.12.4

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
+import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 const external = [
-	...Object.keys(pkg.dependencies)
+	...Object.keys(pkg.dependencies),
+	...Object.keys(pkg.peerDependencies)
 ];
 
 export default [
@@ -12,5 +14,18 @@ export default [
 			file: pkg.browser,
 			format: 'es'
 		}
+	},
+	{
+		input: 'src/server/index.js',
+		external,
+		output: {
+			file: pkg.main,
+			format: 'cjs'
+		},
+		plugins: [
+			babel({
+				exclude: ['node_modules/**']
+			})
+		]
 	}
 ];
---
+++ src/server/index.js
@@ -0,0 +1,1 @@
+export * from './components/CoreTracking.jsx';
