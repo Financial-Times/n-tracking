7ec8fb7278f0ee0c501ad2c4d336c9aacfd9fa1a
Adam Braimbridge <adam.braimbridge@ft.com>
2019-12-23 10:37:55 +0000

    Added stub test for real-user-monitoring-for-performance.js 🐿 v2.12.5

---
+++ src/client/__test__/real-user-monitoring-for-performance.spec.js
@@ -0,0 +1,8 @@
+
+describe('src/client/real-user-monitoring-for-performance.js', () => {
+  describe('.realUserMonitoringForPerformance()', () => {
+    it('initialises o-tracking', () => {
+      expect(false).toEqual(true)
+    })
+  })
+})
