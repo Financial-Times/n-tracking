3f500942283ad3e64defcae633819c4f72f1332b
Matt Hinchliffe <matt.hinchliffe@ft.com>
2020-01-13 12:42:55 +0000

    Add text copy tracker to public interface  🐿 v2.12.4

--- src/client/trackers/index.js
+++ src/client/trackers/index.js
@@ -1,4 +1,3 @@
-// TODO: implement and test trackers copied over from n-ui
-// https://github.com/Financial-Times/n-ui/tree/master/components/n-ui/tracking/ft/events
 export * from './realUserMonitoringForPerformance';
 export * from './pageAttention';
+export * from './textCopying';
